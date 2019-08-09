fd1cd9f0ada3a37f75a4d95c89a66e9639ae3df2
John Luke Saake <johnlukesaake@gmail.com>
2019-08-09 13:22:14 -0700

    Color Changing
    Player color changes on input. Color initialization is set in GameManager.

---
+++ Color Breaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Player player;
+    public Color[] colors; // Location for all colors in the game
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ColorCheck(); // Assert that there are ample colors in the game
+        player = GameObject.FindObjectOfType<Player>();
+        player.SetColors(colors);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Security check to ensure that there are enough colors implemented into the game
+    private void _ColorCheck()
+    {
+        if (colors.Length < 1) // TODO: change to 2 to prevent infinite gameplay
+        {
+            colors = new Color[2];
+            colors[0] = Color.white;
+            colors[1] = Color.black;
+        }
+    }
+}
---
+++ Color Breaker/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Color[] colors; // Handled in the game manager
+    private int colorCount;
+    private int currIndex = 0;
+    MaterialPropertyBlock matBlock;
+    Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        matBlock = new MaterialPropertyBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            ++currIndex;
+            if (currIndex >= colorCount)
+            {
+                currIndex = 0;
+            }
+            matBlock.SetColor("_BaseColor", colors[currIndex]);
+            rend.SetPropertyBlock(matBlock);
+        }
+        
+    }
+
+    public void SetColors(Color[] inputColors)
+    {
+        colors = inputColors;
+        colorCount = inputColors.Length;
+        matBlock.SetColor("_BaseColor", colors[currIndex]);
+        rend.SetPropertyBlock(matBlock);
+    }
+}
